b524af845ddf764b6e83e8fbc5b261edd4905e90
Alex Filatov <tanyaarni@gmail.com>
2019-11-28 18:38:06 +0200

    heroku

--- src/temp.js
+++ src/temp.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 80 || process.env.PORT;
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.end('<div><ul><li><a href="/">home</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
