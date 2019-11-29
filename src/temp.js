7832dd4ed4bdd0d9223efa2a73b015ffd2315863
Alex Filatov <tanyaarni@gmail.com>
2019-11-29 19:26:24 +0200

    Little changes in code.

--- src/temp.js
+++ src/temp.js
@@ -4,7 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
-var ii = 8;
 
 app.get('/', (req, res) => {
   res.end('<div><ul><li><a href="/">home</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
