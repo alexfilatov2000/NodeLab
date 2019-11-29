8e0e28e8af69e5294e6b4bb8bf2945670e10bc17
Alex Filatov <tanyaarni@gmail.com>
2019-11-29 10:35:32 +0200

    husky

--- src/temp.js
+++ src/temp.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
+var ii = 8;
+
 app.get('/', (req, res) => {
   res.end('<div><ul><li><a href="/">home</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
 });
