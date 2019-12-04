f742fab7ea485ae3002ac0f17464c02f82804388
Alex Filatov <tanyaarni@gmail.com>
2019-12-04 12:02:29 +0200

    fjhj

--- src/temp.js
+++ src/temp.js
@@ -4,9 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
-app.get('/about', (req, res) => {
-  res.end('<div><ul><li><a href="/">about</a></li><li><a href="/about">about</a></li></ul><h1>About page</h1></div>');
-});
 
 app.get('/', (req, res) => {
   res.end('<div><ul><li><a href="/">home</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
