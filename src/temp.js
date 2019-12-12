2836b6fe2f9b1b1592326cd1bcfaffffb34222db
Alex Filatov <tanyaarni@gmail.com>
2019-12-12 21:36:27 +0200

    encryption

--- src/temp.js
+++ src/temp.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 80;
 
 
 app.get('/', (req, res) => {
-  res.end('<div><ul><li><a href="/">home</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
+  res.end('<div><ul><li><a href="/">what</a></li><li><a href="/about">about</a></li></ul><h1>Home page</h1></div>');
 });
 
 app.listen(PORT, () => {
